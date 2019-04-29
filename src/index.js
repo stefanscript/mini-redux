65dd85ab83f28ddf3f9358416705b84afed29629
stefman <sha32768@gmail.com>
2019-04-29 21:23:03 +0100

    refactor name

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-let store = {};
+let state = {};
 let rootReducer;
 let listeners = [];
 
 export function createStore(reducer, initState) {
     rootReducer = reducer;
-    store = {...initState};
+    state = {...initState};
 }
 
 export function getStore() {
-    return store;
+    return state;
 }
 
 export function dispatch(action) {
-    const newState = rootReducer(store, action);
-    if(store !== newState){
-        store = newState;
+    const newState = rootReducer(state, action);
+    if(state !== newState){
+        state = newState;
         listeners.map(listener => listener());
     }
 }
